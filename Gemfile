5f0e78b64800083a033c9e57177a92c0a12398a9
Yoshiaki Oshiro <himajin315@gmail.com>
2018-10-20 22:31:48 +0900

    s3にファイルをアップロードする処理を追加

--- Gemfile
+++ Gemfile
@@ -8,3 +8,4 @@ source "https://rubygems.org"
 gem 'sinatra'
 gem 'line-bot-api'
 gem 'pry-byebug'
+gem 'aws-sdk-s3', '~> 1'
--- app_main.rb
+++ app_main.rb
@@ -1,48 +1,44 @@
 require 'sinatra'
 require 'line/bot'
 require 'pry'
+require 'aws-sdk-s3'
 
-# 微小変更部分！確認用。
-get '/' do
-  "Hello world"
+def line_client
+  @client ||= Line::Bot::Client.new { |config|
+    config.channel_secret = ENV['LINE_CHANNEL_SECRET']
+    config.channel_token = ENV['LINE_CHANNEL_TOKEN']
+  }
 end
 
-def client
-  @client ||= Line::Bot::Client.new { |config|
-    config.channel_secret = ENV["LINE_CHANNEL_SECRET"]
-    config.channel_token = ENV["LINE_CHANNEL_TOKEN"]
-  }
+def s3_upload(file)
+  client = Aws::S3::Client.new(
+    access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+    secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
+  )
+
+  s3 = Aws::S3::Resource.new(
+    client: client,
+    region: ENV['AWS_REGION'],
+    endpoint: ENV['AWS_ENDPOINT']
+  )
+
+  obj = s3.bucket(ENV['BUCKET_NAME']).object(Time.now.strftime("%Y%m%d-%H:%M:%S"))
+  obj.put(body: file)
 end
 
 post '/callback' do
   body = request.body.read
 
   signature = request.env['HTTP_X_LINE_SIGNATURE']
-  unless client.validate_signature(body, signature)
+  unless line_client.validate_signature(body, signature)
     error 400 do 'Bad Request' end
   end
 
-  events = client.parse_events_from(body)
+  events = line_client.parse_events_from(body)
   events.each { |event|
-    case event
-    when Line::Bot::Event::Message
-      case event.type
-      when Line::Bot::Event::MessageType::Text
-        message = {
-            type: 'text',
-            text: event.message['text']
-        }
-        client.reply_message(event['replyToken'], message)
-      when Line::Bot::Event::MessageType::Image
-        response = client.get_message_content(event.message['id'])
-        tf = Tempfile.open("content")
-        tf.write(response.body)
-        message = {
-            type: 'text',
-            text: '画像送信完了'
-        }
-        client.reply_message(event['replyToken'], message)
-      end
+    if event.type == Line::Bot::Event::MessageType::Image
+      response = line_client.get_message_content(event.message['id'])
+      s3_upload(response.body)
     end
   }
 
